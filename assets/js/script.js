6f85f4b9717e96a3a92014cc75907ad7214bf73d
amiedawn <amie.d.carroll@gmail.com>
2020-10-29 15:51:14 -0600

    working apikey to retrieve daily weather

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var cityInput = "San Diego";
+var apiKey = "fc812994b97935be7c26648fa44398a1";
+
+var collectWeatherInfo = function() {
+// format api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey;
+
+// make a request to the URL
+fetch(apiUrl)
+  .then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    } else {
+      // if you try to search for a city that doesn't exist
+      alert("Error: " + response.statusText);
+    }
+  })
+  .catch(function(error) {
+    // catch is chained to the then method
+    alert("Unable to connect to Open Weather Map");
+  })
+};  
+  // .then(response => response.json())
+  // .then(function (data) {
+  //   console.log(data)
+  // });
+
+collectWeatherInfo();  
