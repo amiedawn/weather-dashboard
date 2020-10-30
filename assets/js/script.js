b8db846ef16290ede03251f3a1d826f92e435277
amiedawn <amie.d.carroll@gmail.com>
2020-10-30 15:15:03 -0600

    save for learning assistant to help with date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,93 @@
-var inputSearchEl = document.querySelector("#input-search");
-var cityInputEl = document.querySelector("#city");
-var cityContainerEl = document.querySelector("#city-container");
-var apiKey = "fc812994b97935be7c26648fa44398a1";
 
-var collectWeatherInfo = function(city) {
-// format api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + apiKey;
 
-// make a request to the URL
+  var inputSearchEl = document.querySelector("#input-search");
+  var cityInputEl = document.querySelector("#city");
+  var cityContainerEl = document.querySelector("#city-container");
+  var apiKey = "fc812994b97935be7c26648fa44398a1";
 
-fetch(apiUrl) 
-  .then(function(response) {
-    if (response.ok) {
-      response.json()
-      
-      .then(function(data) {
-        //displayDailyWeather(data, city);
-        //console.log(data);
 
-        // display current day 
-        var today = moment().format("M/D/YYYY");
-        var $subtitleDate = $("#currentDay");
+  // // store and display recent searches in local storage
+  // var $city = $(".form-input");
+  // debugger;
 
-        $subtitleDate.text(today);
-        debugger;
-        $(".city").html("<h1>" + data.name + " (" + $subtitleDate + ")");
-        $(".temp").text("Temperature: " + data.main.temp + "degrees F");
-        $(".humidity").text("Humidity: " + data.main.humidity + "%");
-        $(".wind").text("Wind Speed: " + data.wind.speed + "&units=imperial" + " MPH"); //**convert to imperial */
-      });
+  // $("button").on("click", function() {
+  //   localStorage.setItem(".search-line"), JSON.stringify($city.val());
+  //   var persistCity  = JSON.parse(localStorage.getItem("search-line"));
+  //   $(".form-input").val(persistCity);
+  // });
+
+  var collectWeatherInfo = function(city) {
+  // format api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + apiKey;
+
+  // make a request to the URL
+
+  fetch(apiUrl) 
+    .then(function(response) {
+      if (response.ok) {
+        response.json()
+        
+        .then(function(data) {
+          //displayDailyWeather(data, city);
+          //console.log(data);
+
+          // display current day 
+          var today = moment().format("M/D/YYYY");
+          var $subtitleDate = $("#currentDay");
+
+          $subtitleDate.text(today);
+        
+          $(".city").html("<h1>" + data.name + " (" + $subtitleDate.text(today) + ")");
+          $(".temp").text("Temperature: " + data.main.temp + "° F");
+          $(".humidity").text("Humidity: " + data.main.humidity + "%");
+          $(".wind").text("Wind Speed: " + data.wind.speed + "&units=imperial" + " MPH"); //**convert to imperial */
+        });
+      } else {
+        // if you try to search for a city that doesn't exist
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      // catch is chained to the then method
+      alert("Unable to connect to Open Weather Map");
+    })
+  };  
+
+  var searchSubmitHandler = function(event) {
+    event.preventDefault();
+
+    // get value from input element
+    var city = cityInputEl.value.trim();
+
+    if (city) {
+      collectWeatherInfo(city);
+      //cityInputEl.value = "";
     } else {
-      // if you try to search for a city that doesn't exist
-      alert("Error: " + response.statusText);
+
+      alert("Please enter a city you would like to know the weather for.");
     }
-  })
-  .catch(function(error) {
-    // catch is chained to the then method
-    alert("Unable to connect to Open Weather Map");
-  })
-};  
 
-var searchSubmitHandler = function(event) {
-  event.preventDefault();
+    console.log(event);
+  };
 
-  // get value from input element
-  var city = cityInputEl.value.trim();
+  var displayDailyWeather = function(cities) {
+    //check if the api returned any repos
+    if (cities.length === 0) {
+      cityContainerEl.textContent = "No information available for this city.";
+      return;
+    }
 
-  if (city) {
-    collectWeatherInfo(city);
-    //cityInputEl.value = "";
-  } else {
+    // loop over cities
+    for (var i = 0; i < cities.length; i++) {
+      // create a row for each city
+      var cityEl = document.createElement("a");
+      cityEl.classList = "list-item flex-row justify-space-between align-left";
 
-    alert("Please enter a city you would like to know the weather for.");
-  }
-
-  console.log(event);
-};
-
-// var displayDailyWeather = function(cities) {
-//   //check if the api returned any repos
-//   if (cities.length === 0) {
-//     cityContainerEl.textContent = "No information available for this city.";
-//     return;
-//   }
-
-//   // loop over cities
-//   for (var i = 0; i < cities.length; i++) {
-//     // create a row for each city
-//     var cityEl = document.createElement("a");
-//     cityEl.classList = "list-item flex-row justify-space-between align-left";
-
-//     // append to the list
-//     cityContainerEl.appendChild(cityEl);
-//   }
-// };
+      // append to the list
+      cityContainerEl.appendChild(cityEl);
+    }
+  };
 
 
-inputSearchEl.addEventListener("submit", searchSubmitHandler);
+ inputSearchEl.addEventListener("submit", searchSubmitHandler); //--commenting because of onclick
+
