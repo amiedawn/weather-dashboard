f5a75054ca84d535ec0073c2c6e6c2e7c86377a5
amiedawn <amie.d.carroll@gmail.com>
2020-11-01 18:49:56 -0700

    need to fix persist length, persist, history, and link

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ var searchSubmitHandler = function (event) {
 };
 
 // var collectWeatherInfo = function (city) { <= don't think I need to pass in city anymore
-var collectWeatherInfo = function () {  
+var collectWeatherInfo = function () {
 
   // get value from input element
   var city = cityInputEl.value.trim();
@@ -28,7 +28,7 @@ var collectWeatherInfo = function () {
 
   //Daily format: api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
   var apiUrlDaily = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + apiKey;
-  
+
   // make a request to the daily weather API
   fetch(apiUrlDaily)
     .then(response => response.json())
@@ -37,167 +37,201 @@ var collectWeatherInfo = function () {
       console.log("weather", data.weather);
       renderList(data);
 
+
+
+
+
+
+
+
+
       // create list-group table of cities searched for
-      function renderList(data) {
-        var cityList = $(".list-group");
-        console.log("data.name", data.name)
-        debugger;
-        for (var i = 0; i < data.weather.length; i++) {
-          var result = data.name;
-          console.log("**result", result)
-          var listItem = $("<a class='list-group-item list-group-item-action' />");
-          listItem.text(result.name);
-          listItem.attr("href", result.index);
-          cityList.append(result);
-        }
+      // function renderList(data) {
+      //   var cityList = $(".list-group");
+      //   console.log("data.name", data.name)
+      //   debugger;
+      //   for (var i = 0; i < data.weather.length; i++) {
+      //     var result = data.name;
+      //     console.log("**result", result)
+      //     var listItem = $("<a class='list-group-item list-group-item-action' />");
+      //     listItem.text(result.name);
+      //     listItem.attr("href", result.index);
+      //  //   cityList.append(result);
+      //   }
 
-        $(".list-group-item").on("click", clickList);
-        localStorage.setItem("City1", result);
-      }
+      //  $(".list-group-item").on("click", clickList);
+      //localStorage.setItem("City1", JSON.stringify(result));
+      //  }
 
-      function clickList(event) {
-        event.preventDefault();
-        $(".list-group-item").removeClass("active");
 
-        var item = $(this);
-        item.addClass("active");
-      }
 
-      var latCoord = data.coord.lat;
-      var lonCoord = data.coord.lon;
-      console.log("latCoord", latCoord);
-      console.log("lonCoord", lonCoord);
 
-      //var apiUrlUV = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=" + apiKey;
-      var apiUrlUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latCoord + "&lon=" + lonCoord + "&appid=" + apiKey;
 
-      fetch(apiUrlUV)
-        .then(response => response.json())
-        .then(function (data) {
-          console.log("UV Index", data.value)
-          var uvIndex = data.value;
-          if (uvIndex <= 2) {
-            $(".badge-success").text("UV Index: " + uvIndex);
+    
+
+  var latCoord = data.coord.lat;
+  var lonCoord = data.coord.lon;
+  console.log("latCoord", latCoord);
+  console.log("lonCoord", lonCoord);
+
+  //var apiUrlUV = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=" + apiKey;
+  var apiUrlUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latCoord + "&lon=" + lonCoord + "&appid=" + apiKey;
+
+  fetch(apiUrlUV)
+    .then(response => response.json())
+    .then(function (data) {
+      console.log("UV Index", data.value)
+      var uvIndex = data.value;
+      if (uvIndex <= 2) {
+        $(".badge-success").text("UV Index: " + uvIndex);
+        $(".badge-warning").hide(); //show the right badge
+        $(".badge-success").show(); //show the right badge
+        $(".badge-danger").hide(); //show the right badge
+      } else {
+        if (uvIndex > 2 && uvIndex <= 7) {
+          $(".badge-warning").text("UV Index: " + uvIndex);
+          $(".badge-warning").show(); //show the right badge
+          $(".badge-success").hide(); //show the right badge
+          $(".badge-danger").hide(); //show the right badge
+        } else {
+          if (uvIndex > 7) {
+            $(".badge-danger").text("UV Index: " + uvIndex);
             $(".badge-warning").hide(); //show the right badge
-            $(".badge-success").show(); //show the right badge
-            $(".badge-danger").hide(); //show the right badge
-          } else {
-            if (uvIndex > 2 && uvIndex <= 7) {
-              $(".badge-warning").text("UV Index: " + uvIndex);
-              $(".badge-warning").show(); //show the right badge
-              $(".badge-success").hide(); //show the right badge
-              $(".badge-danger").hide(); //show the right badge
-            } else {
-              if (uvIndex > 7) {
-                $(".badge-danger").text("UV Index: " + uvIndex);
-                $(".badge-warning").hide(); //show the right badge
-                $(".badge-success").hide(); //show the right badge
-                $(".badge-danger").show(); //show the right badge
-              }
-            }
+            $(".badge-success").hide(); //show the right badge
+            $(".badge-danger").show(); //show the right badge
           }
-        })
+        }
+      }
+    })
 
-      // display current day 
-      var today = moment().format("M/D/YYYY");
+  // display current day 
+  var today = moment().format("M/D/YYYY");
 
-      $(".current-card").removeClass("d-none"); // shows card after being hidden on page load
-      $(".five-day-cards").addClass("d-none");
-      $(".badge").display = "none"; // hide badges on page load
+  $(".current-card").removeClass("d-none"); // shows card after being hidden on page load
+  $(".five-day-cards").addClass("d-none");
+  $(".badge").display = "none"; // hide badges on page load
 
-      // format header of current day
-      $(".city").html("<h1>" + data.name + " (" + today + ")" + "</h1>");
-      $(".icon").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+  // format header of current day
+  $(".city").html("<h1>" + data.name + " (" + today + ")" + "</h1>");
+  $(".icon").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
 
-      // format rest of current day
-      $(".temp").text("Temperature: " + Math.floor(data.main.temp) + "° F");
-      $(".humidity").text("Humidity: " + data.main.humidity + "%");
-      $(".wind").text("Wind Speed: " + data.wind.speed + " MPH");
-      
-      // api.openweathermap.org / data / 2.5 / forecast ? q = { city name } & appid={ API key }
-      var api5Day = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&appid=" + apiKey;
+  // format rest of current day
+  $(".temp").text("Temperature: " + Math.floor(data.main.temp) + "° F");
+  $(".humidity").text("Humidity: " + data.main.humidity + "%");
+  $(".wind").text("Wind Speed: " + data.wind.speed + " MPH");
 
-      // fetch for 5 day forecast
-      fetch(api5Day)
-        .then(response => response.json())
-        .then(function (data) {
-          console.log(data);
+  // api.openweathermap.org / data / 2.5 / forecast ? q = { city name } & appid={ API key }
+  var api5Day = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&appid=" + apiKey;
 
-          // set variables for reading dates in 5 day forecast
-          var forecastDay1 = data.list[6].dt_txt;
-          console.log("forecastDay1", forecastDay1, typeof forecastDay1);
-          var responseDay1 = moment(forecastDay1).format("M/D/YYYY")
-          console.log("responseDay1", responseDay1, typeof responseDay1);
+  // fetch for 5 day forecast
+  fetch(api5Day)
+    .then(response => response.json())
+    .then(function (data) {
+      console.log(data);
 
-          var forecastDay2 = data.list[14].dt_txt;
-          console.log("forecastDay2", forecastDay2, typeof forecastDay2);
-          var responseDay2 = moment(forecastDay2).format("M/D/YYYY")
-          console.log("responseDay2", responseDay2, typeof responseDay2);
+      // set variables for reading dates in 5 day forecast
+      var forecastDay1 = data.list[6].dt_txt;
+      console.log("forecastDay1", forecastDay1, typeof forecastDay1);
+      var responseDay1 = moment(forecastDay1).format("M/D/YYYY")
+      console.log("responseDay1", responseDay1, typeof responseDay1);
 
-          var forecastDay3 = data.list[22].dt_txt;
-          console.log("forecastDay3", forecastDay3, typeof forecastDay3);
-          var responseDay3 = moment(forecastDay3).format("M/D/YYYY")
-          console.log("responseDay3", responseDay3, typeof responseDay3);
+      var forecastDay2 = data.list[14].dt_txt;
+      console.log("forecastDay2", forecastDay2, typeof forecastDay2);
+      var responseDay2 = moment(forecastDay2).format("M/D/YYYY")
+      console.log("responseDay2", responseDay2, typeof responseDay2);
 
-          var forecastDay4 = data.list[30].dt_txt;
-          console.log("forecastDay4", forecastDay4, typeof forecastDay4);
-          var responseDay4 = moment(forecastDay4).format("M/D/YYYY")
-          console.log("responseDay4", responseDay4, typeof responseDay4);
+      var forecastDay3 = data.list[22].dt_txt;
+      console.log("forecastDay3", forecastDay3, typeof forecastDay3);
+      var responseDay3 = moment(forecastDay3).format("M/D/YYYY")
+      console.log("responseDay3", responseDay3, typeof responseDay3);
 
-          var forecastDay5 = data.list[38].dt_txt;
-          console.log("forecastDay5", forecastDay5, typeof forecastDay5);
-          var responseDay5 = moment(forecastDay5).format("M/D/YYYY")
-          console.log("responseDay5", responseDay5, typeof responseDay5);
+      var forecastDay4 = data.list[30].dt_txt;
+      console.log("forecastDay4", forecastDay4, typeof forecastDay4);
+      var responseDay4 = moment(forecastDay4).format("M/D/YYYY")
+      console.log("responseDay4", responseDay4, typeof responseDay4);
 
-          $(".five-day-cards").removeClass("d-none"); // shows card after being hidden on page load
-          // display header date of 5 day forcast cards
-          $("#date1").text(responseDay1);
-          $("#date2").text(responseDay2);
-          $("#date3").text(responseDay3);
-          $("#date4").text(responseDay4);
-          $("#date5").text(responseDay5);
+      var forecastDay5 = data.list[38].dt_txt;
+      console.log("forecastDay5", forecastDay5, typeof forecastDay5);
+      var responseDay5 = moment(forecastDay5).format("M/D/YYYY")
+      console.log("responseDay5", responseDay5, typeof responseDay5);
 
-          // display and format icons for each day
-          $("#icon1").attr("src", "http://openweathermap.org/img/w/" + data.list[6].weather[0].icon + ".png");
-          $("#icon2").attr("src", "http://openweathermap.org/img/w/" + data.list[14].weather[0].icon + ".png");
-          $("#icon3").attr("src", "http://openweathermap.org/img/w/" + data.list[22].weather[0].icon + ".png");
-          $("#icon4").attr("src", "http://openweathermap.org/img/w/" + data.list[30].weather[0].icon + ".png");
-          $("#icon5").attr("src", "http://openweathermap.org/img/w/" + data.list[38].weather[0].icon + ".png");
+      $(".five-day-cards").removeClass("d-none"); // shows card after being hidden on page load
+      // display header date of 5 day forcast cards
+      $("#date1").text(responseDay1);
+      $("#date2").text(responseDay2);
+      $("#date3").text(responseDay3);
+      $("#date4").text(responseDay4);
+      $("#date5").text(responseDay5);
 
-          // display and format temperature for each day
-          $("#temp1").text("Temp: " + Math.floor(data.list[6].main.temp) + "° F");
-          $("#temp2").text("Temp: " + Math.floor(data.list[14].main.temp) + "° F");
-          $("#temp3").text("Temp: " + Math.floor(data.list[22].main.temp) + "° F");
-          $("#temp4").text("Temp: " + Math.floor(data.list[30].main.temp) + "° F");
-          $("#temp5").text("Temp: " + Math.floor(data.list[38].main.temp) + "° F");
+      // display and format icons for each day
+      $("#icon1").attr("src", "http://openweathermap.org/img/w/" + data.list[6].weather[0].icon + ".png");
+      $("#icon2").attr("src", "http://openweathermap.org/img/w/" + data.list[14].weather[0].icon + ".png");
+      $("#icon3").attr("src", "http://openweathermap.org/img/w/" + data.list[22].weather[0].icon + ".png");
+      $("#icon4").attr("src", "http://openweathermap.org/img/w/" + data.list[30].weather[0].icon + ".png");
+      $("#icon5").attr("src", "http://openweathermap.org/img/w/" + data.list[38].weather[0].icon + ".png");
 
-          // display and format humidity for each day
-          $("#hum1").text("Humidity: " + data.list[6].main.humidity + "%");
-          $("#hum2").text("Humidity: " + data.list[14].main.humidity + "%");
-          $("#hum3").text("Humidity: " + data.list[22].main.humidity + "%");
-          $("#hum4").text("Humidity: " + data.list[30].main.humidity + "%");
-          $("#hum5").text("Humidity: " + data.list[38].main.humidity + "%");
-        })
+      // display and format temperature for each day
+      $("#temp1").text("Temp: " + Math.floor(data.list[6].main.temp) + "° F");
+      $("#temp2").text("Temp: " + Math.floor(data.list[14].main.temp) + "° F");
+      $("#temp3").text("Temp: " + Math.floor(data.list[22].main.temp) + "° F");
+      $("#temp4").text("Temp: " + Math.floor(data.list[30].main.temp) + "° F");
+      $("#temp5").text("Temp: " + Math.floor(data.list[38].main.temp) + "° F");
 
-        .catch(function (error) {
-          // if you try to search for a city that doesn't exist
-          alert("Error: " + response.statusText);
-        });
-      retrieveDisplayStorage();
+      // display and format humidity for each day
+      $("#hum1").text("Humidity: " + data.list[6].main.humidity + "%");
+      $("#hum2").text("Humidity: " + data.list[14].main.humidity + "%");
+      $("#hum3").text("Humidity: " + data.list[22].main.humidity + "%");
+      $("#hum4").text("Humidity: " + data.list[30].main.humidity + "%");
+      $("#hum5").text("Humidity: " + data.list[38].main.humidity + "%");
+    })
+
+    .catch(function (error) {
+      // if you try to search for a city that doesn't exist
+      alert("Error: " + response.statusText);
     });
-  
-  
+  retrieveDisplayStorage();
+});
 };
 
-var retrieveDisplayStorage = function() {
-  var persist1 = localStorage.getItem("City1");
-  $(".list-group").append(`<a href="#" class="list-group-item list-group-item-action">${persist1}</a>`);
- // $(".list-group").append(`<a href="#" class="list-group-item list-group-item-action">${persist1}</a>`);
-  console.log("city from storage", persist1);
-  console.log("city from list-group", $(".list-group").val(persist1));
+// create list-group table of cities searched for
+function renderList(data) {
+  var cityList = $(".list-group");
+  //   console.log("data.name", data.name)
+  //   debugger;
+  //   for (var i = 0; i < data.weather.length; i++) {
+  //     var result = data.name;
+  //     console.log("**result", result)
+  //     var listItem = $("<a class='list-group-item list-group-item-action' />");
+  //     listItem.text(result.name);
+  //     listItem.attr("href", result.index);
+  //     //   cityList.append(result);
+  //   }
+
+  $(".list-group-item").on("click", clickList);
+  localStorage.setItem("City1", JSON.stringify(data.name));
+};
+
+function clickList(event) {
+  event.preventDefault();
+  $(".list-group-item").removeClass("active");
+
+  var item = $(this);
+  item.addClass("active");
+};
+
+var retrieveDisplayStorage = function () { // <= works for list
+  debugger;
+ var persist1 = JSON.parse(localStorage.getItem("City1"));
+ //persist1 = [];
+  for (var i = 0; i < persist1.length; i++) {
+    $(".list-group").append(`<a href="#" class="list-group-item list-group-item-action">${persist1}</a>`);
+    // $(".list-group").append(<a href="#" class="list-group-item list-group-item-action"> + $(persist1) + "</a>");
+    console.log("city from storage", persist1);
+    console.log("city from list-group", $(".list-group").val(persist1));
+  }
 };
 
 retrieveDisplayStorage();
 
-inputSearchEl.addEventListener("submit", searchSubmitHandler); //--commenting because of onclick
+inputSearchEl.addEventListener("submit", searchSubmitHandler);
 
