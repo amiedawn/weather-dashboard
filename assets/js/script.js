0372c8e76047116d7ba899d62dfb6bcb2a50e874
amiedawn <amie.d.carroll@gmail.com>
2020-11-03 14:19:13 -0700

    functionality complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,41 @@
 var inputSearchEl = document.querySelector("#input-search");
-console.log(inputSearchEl);
-
 var cityInputEl = document.querySelector("#city");
-console.log(cityInputEl);
-
 var cityContainerEl = document.querySelector("#city-container");
-//var apiKey = "fc812994b97935be7c26648fa44398a1";
 var apiKey = "975f21cfd36254818b964487770790a3";
-
-
 var persist1 = JSON.parse(localStorage.getItem("persist1")) || [];
-
 
 var searchSubmitHandler = function (event) {
   event.preventDefault();
 
   // get value from input element
- debugger;
-
   var city = cityInputEl.value.trim();
-  console.log(city);
 
   if (city) {
     collectWeatherInfo(city);
   } else {
     alert("Please enter a city you would like to know the weather for.");
   }
-  console.log(event);
 };
 
 var collectWeatherInfo = function (city) {
-  // get value from input element <== moved these three lines to searchSubmitHandler
- // var city = cityInputEl.value.trim();
- // console.log(city);
-
-  var apiUrlDaily = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + apiKey;
+var apiUrlDaily = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + apiKey;
 
   // make a request to the daily weather API
   fetch(apiUrlDaily)
     .then(response => response.json())
     .then(function (data) {
-      console.log(data);
       persist1.unshift(data.name);
       localStorage.setItem("Search History", JSON.stringify(persist1));
-      console.log(persist1);
-      console.log("name", data.name);
-      console.log("weather", data.weather);
       displaySearchHistory();
 
       // start working on UV Index fetch
       var latCoord = data.coord.lat;
       var lonCoord = data.coord.lon;
-      console.log("latCoord", latCoord);
-      console.log("lonCoord", lonCoord);
-
       var apiUrlUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latCoord + "&lon=" + lonCoord + "&appid=" + apiKey;
 
       fetch(apiUrlUV)
         .then(response => response.json())
         .then(function (data) {
-          console.log("UV Index", data.value)
           var uvIndex = data.value;
           if (uvIndex <= 2) {
             $(".badge-success").text("UV Index: " + uvIndex);
@@ -105,36 +81,25 @@ var collectWeatherInfo = function (city) {
       fetch(api5Day)
         .then(response => response.json())
         .then(function (data) {
-          console.log(data);
-
           // set variables for reading dates in 5 day forecast
           var forecastDay1 = data.list[6].dt_txt;
-          console.log("forecastDay1", forecastDay1, typeof forecastDay1);
           var responseDay1 = moment(forecastDay1).format("M/D/YYYY")
-          console.log("responseDay1", responseDay1, typeof responseDay1);
-
+          
           var forecastDay2 = data.list[14].dt_txt;
-          console.log("forecastDay2", forecastDay2, typeof forecastDay2);
           var responseDay2 = moment(forecastDay2).format("M/D/YYYY")
-          console.log("responseDay2", responseDay2, typeof responseDay2);
-
+          
           var forecastDay3 = data.list[22].dt_txt;
-          console.log("forecastDay3", forecastDay3, typeof forecastDay3);
           var responseDay3 = moment(forecastDay3).format("M/D/YYYY")
-          console.log("responseDay3", responseDay3, typeof responseDay3);
-
+          
           var forecastDay4 = data.list[30].dt_txt;
-          console.log("forecastDay4", forecastDay4, typeof forecastDay4);
           var responseDay4 = moment(forecastDay4).format("M/D/YYYY")
-          console.log("responseDay4", responseDay4, typeof responseDay4);
-
+          
           var forecastDay5 = data.list[38].dt_txt;
-          console.log("forecastDay5", forecastDay5, typeof forecastDay5);
           var responseDay5 = moment(forecastDay5).format("M/D/YYYY")
-          console.log("responseDay5", responseDay5, typeof responseDay5);
-
+          
           $(".card-group").removeClass("d-none"); // shows card title after being hidden on page load
           $(".five-day-cards").removeClass("d-none"); // shows cards after being hidden on page load
+         
           // display header date of 5 day forecast cards
           $("#date1").text(responseDay1);
           $("#date2").text(responseDay2);
@@ -174,21 +139,16 @@ var collectWeatherInfo = function (city) {
 
 function displaySearchHistory() {
   $("#search-history").empty();
-  debugger;
   
   for (var i = 0; i < persist1.length; i++) {
-    var listItem = $('<li class="list-group-item">' + persist1[i] + "</li>");
+    // let instead of var allows to the city clicked, not just the latest one
+    let listItem = $('<li class="list-group-item">' + persist1[i] + "</li>");
      $("#search-history").append(listItem);
     listItem.addClass("active");
-        
-  //$(" li-class").on("click", "li", function() {
-    //listItem.addEventListener("click", function() {
-      $(listItem).on("click", function() {
-    //listItem.click(function() {
-    collectWeatherInfo(persist1[i]);
-    //  $(".list-group-item").removeClass("active");
-    //(".list-group-item").removeClass("active");
-        (listItem).removeClass("active");
+    
+    // allows click for list
+    $(listItem).on("click", function() {
+    collectWeatherInfo(listItem.text());
     })
   }
 };
@@ -199,6 +159,3 @@ function displaySearchHistory() {
 
   // set city to kickoff weather search determined by city entered
   inputSearchEl.addEventListener("submit", searchSubmitHandler);
-
-  
-//document.getElementById("search-history").addEventListener("click", (collectWeatherInfo(persist1)));
